dad3d4bd016622f3ef7955c4bc9e892560aadaf9
delicniradidevjsonfieserverconfiradi <42246911+delicniradidevjsonfieserverconfiradi@users.noreply.github.com>
2018-08-12 11:12:57 -0500

    Update create-task.bat

--- Scripts/create-task.bat
+++ Scripts/create-task.bat
@@ -1,4 +1,4 @@
 @echo off
 SchTasks /Delete /TN "Radio-Delicias" /F
-SchTasks /Create /NP /SC MINUTE /MO 10 /TN "Radio-Delicias" /TR "C:\Radio-Delicias\iniciar-transmision.bat"
+SchTasks /Create /NP /SC MINUTE /MO 10 /TN "Radio-Delicias" /TR "C:\Radio-Delicias\server\start.bat"
 exit
